ee406ef0c2d94d1e0b8f7db232355a432727604c
Lê Đức Ngọc Bảo <baoldn@aipower.com.vn>
2025-10-30 17:04:31 +0700

    feat: Computed Fields, Dependencies & Inverse Function. Have bug in inverse func of validity

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
@@ -56,3 +56,17 @@ class EstateProperty(models.Model):
     tag_ids = fields.Many2many('estate.property.tag', string="Tags")
 
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
+
+    best_price = fields.Float(string='Best Offer', compute='_compute_best_price')
+
+    total_area = fields.Integer(string='Total Area (sqm)', compute='_compute_total_area')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'), default=0.0)
--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from datetime import timedelta
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -14,3 +15,22 @@ class EstatePropertyOffer(models.Model):
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
 
     property_id = fields.Many2one('estate.property', string="Property", required=True)
+
+    validity = fields.Integer(string="Validity (days)", default=7, inverse='_inverse_validity')
+
+    date_deadline = fields.Date(string="Deadline", compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = record.create_date + timedelta(days=record.validity)            
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.date_deadline = record.create_date - timedelta(days=record.validity)  
+
+    @api.depends('date_deadline')
+    def _inverse_validity(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date).days
+    
