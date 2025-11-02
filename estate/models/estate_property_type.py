c75753711ca00bbe1c5520f09c275a3d25f08fad
LeNgocBao145 <himagito@gmail.com>
2025-11-02 07:45:45 +0700

    feat: add inline views and refactor stat button

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -9,6 +9,7 @@ class EstatePropertyType(models.Model):
     name = fields.Char(required=True, string="Type")
     offer_ids = fields.One2many('estate.property.offer', 'property_type_id')
     offer_count = fields.Integer(compute="_compute_offer_count")
+    property_ids = fields.One2many('estate.property', 'property_type_id')
 
     _sql_constraints = [
         ('unique_type_name', 'unique(name)', 'Property type name must be unique!')
@@ -18,14 +19,3 @@ class EstatePropertyType(models.Model):
     def _compute_offer_count(self):
         for record in self:
             record.offer_count = len(record.offer_ids)
-
-    def action_view_offers(self):
-       self.ensure_one()
-       return {
-           "type": "ir.actions.act_window",
-           "name": "Offers",
-           "res_model": "estate.property.offer",
-           "view_mode": "list,form",
-           "domain": [("property_type_id", "=", self.id)],
-           "context": {"default_property_type_id": self.id},
-       }
